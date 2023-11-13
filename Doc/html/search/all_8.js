22c10eb18ece1cbd2011b53ca91a6d56ddd463f8
Александр Фролов <alex.froller@gmail.com>
2023-11-14 00:33:52 +0300

    Doxymentation: main page restored

---
+++ Doc/html/search/all_8.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['sco_0',['sco',['../d4/dc0/structBMP180__ControlRegisterTypeDef.html#aeb0ed2d280f1af9cafca92d1748aa930',1,'BMP180_ControlRegisterTypeDef']]],
+  ['sensor_1',['Sensor',['../d4/d99/structBMP180__HandleTypeDef.html#a0a7791f91e6d8d5716b76b7737707f9c',1,'BMP180_HandleTypeDef']]],
+  ['stm32_2dbmp180_2',['stm32-bmp180',['../index.html',1,'']]]
+];
--- Doc/html/search/searchdata.js
+++ Doc/html/search/searchdata.js
@@ -1,13 +1,14 @@
 var indexSectionsWithContent =
 {
-  0: "abcdimos",
+  0: "abcdimors",
   1: "b",
-  2: "b",
+  2: "br",
   3: "b",
   4: "abcdimos",
   5: "b",
   6: "b",
-  7: "i"
+  7: "i",
+  8: "s"
 };
 
 var indexSectionNames =
@@ -19,7 +20,8 @@ var indexSectionNames =
   4: "variables",
   5: "enums",
   6: "enumvalues",
-  7: "defines"
+  7: "defines",
+  8: "pages"
 };
 
 var indexSectionLabels =
@@ -31,6 +33,7 @@ var indexSectionLabels =
   4: "Variables",
   5: "Enumerations",
   6: "Enumerator",
-  7: "Macros"
+  7: "Macros",
+  8: "Pages"
 };
 
